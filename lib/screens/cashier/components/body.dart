a8a54635e077b6b1ed0f8fbbc7a48bd9e0855e9d
Ayman Mubarak <cs.aymanmubarak@gmail.com>
2019-09-02 21:21:56 +0200

    ok

--- lib/screens/cashier/components/body.dart
+++ lib/screens/cashier/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smart_epay/Themes/constants.dart';
 
 class Body extends StatefulWidget {
@@ -106,19 +107,153 @@ class _BodyState extends State<Body> {
               child: Container(
                 color: Colors.transparent,
                 child: Container(
-                    decoration: BoxDecoration(
-                        color: appcolor,
-                        borderRadius: BorderRadius.only(
-                            topLeft: const Radius.circular(15.0),
-                            topRight: const Radius.circular(15.0))),
-                    child: Center(
-                      child: Text("Hi modal sheet",style: TextStyle(color: Colors.white,fontSize: 18),),
-                    )),
+                  decoration: BoxDecoration(
+                      color: appcolor,
+                      borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(15.0),
+                          topRight: const Radius.circular(15.0))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Flexible(
+                        child: Text(
+                          "Current Balance",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          "28000 AED",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
             Flexible(
               flex: 2,
-              child: Text('data'),
+              child: Column(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      decoration: new BoxDecoration(
+                        border: new Border(
+                          bottom: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Flexible(
+                            flex: 2,
+                            child: Column(
+                              children: <Widget>[
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      'Cashier',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey),
+                                    ),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: Text(
+                                    'Ayman Mubarak',
+                                    textAlign: TextAlign.center,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Flexible(
+                              flex: 1,
+                              child: VerticalDivider(
+                                color: Colors.grey,
+                                width: 0,
+                              )),
+                          Flexible(
+                            flex: 2,
+                            child: Column(
+                              children: <Widget>[
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      'Merchent',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey),
+                                    ),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: Text(
+                                    'Tom & Jery',
+                                    textAlign: TextAlign.center,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(top: 30, left: 10, right: 10),
+                      child: Column(
+                        children: <Widget>[
+                          Flexible(
+                            child: TextField(
+                              textAlign: TextAlign.center,
+                              onChanged: (String value) {},
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                  hintText: 'Amount To Collect',
+                                  hintStyle: TextStyle(color: appcolor)),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Flexible(
+                            child: RaisedButton(
+                              padding: EdgeInsets.symmetric(horizontal: 40),
+                              color: appcolor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                              child: Text(
+                                'Collect Now',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                              onPressed: () {},
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
